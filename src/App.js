6a1eff0b42bc4aefc5d964161094b5b4cef5b641
psvysakh <psvysakh2@gmail.com>
2020-07-14 12:47:09 +0530

    commiting files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './components/Homepage';
+function App() {
+  return (
+    <div className="App">
+      <h1>Shop World!</h1>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Homepage = ()=>{
+    return(
+        <div className="Home-page">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h3 className="title">Hats</h3>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
